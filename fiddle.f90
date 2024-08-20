f43210e5f65eec7c6c69ad7d8864b6bbe576eaa9
David Lecomber <david.lecomber@arm.com>
2024-08-20 12:38:04 +0100

    add fortran part

---
+++ fiddle.f90
@@ -0,0 +1,5 @@
+function fiddlesticks(r)
+  REAL :: r
+
+  fiddlesticks = 1 - r
+end function fiddlesticks
--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
+extern "C" {
+  void fiddlesticks();
+}
 
 int main() {
 
 	cout << "Hello world" << endl;
+        fiddlesticks()
 }
